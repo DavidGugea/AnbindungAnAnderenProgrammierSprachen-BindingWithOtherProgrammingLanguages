f848ebec9b7d187e674605dd688c63e2676901bd
David <davidgugea@yahoo.com>
2020-05-23 12:40:59 +0200

    Building DLLs in C++ ( C doesn't work for Visual Studio ) - Not a valid Win32 Application issue

---
+++ DLL_TEST_CPP_2/DLL_TEST_CPP_2/DLL_TEST_CPP_2.cpp
@@ -0,0 +1,15 @@
+// DLL_TEST_CPP_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World !" << endl;
+
+
+	return 0;
+}
---
+++ DLL_TEST_CPP_3/DLL_TEST_CPP_3/DLL_TEST_CPP_3.cpp
@@ -0,0 +1,10 @@
+// DLL_TEST_CPP_3.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
+int main() 
+{
+	return 0;
+}
