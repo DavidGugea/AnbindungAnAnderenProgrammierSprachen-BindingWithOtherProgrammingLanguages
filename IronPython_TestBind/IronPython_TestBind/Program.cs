b5e9ac6bcd4bfa8b8722776783bf356b69de3fe5
David <davidgugea@yahoo.com>
2020-05-26 19:07:10 +0200

    Using IronPython to bind C# application to script.py - Works with no issues using IronPython 2.7.10

---
+++ IronPython_TestBind/IronPython_TestBind/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+
+namespace IronPython_TestBind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            engine.ExecuteFile("script.py", scope);
+            System.Console.WriteLine(
+                engine.Operations.Invoke(scope.GetVariable("quadrat"), 5)
+            );
+
+            System.Console.ReadLine();
+        }
+    }
+}
